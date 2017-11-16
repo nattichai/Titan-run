8d0521cd8602e7bf10918becd396f5565f1165ff
Danoosit Techapetpaiboon <danoosit@gmail.com>
2017-11-16 15:46:00 +0700

    addskill

---
+++ src/entity/skill/FireBall.java
@@ -0,0 +1,51 @@
+package entity.skill;
+
+import entity.Entity;
+import property.Animatable;
+import property.Movable;
+import utility.Pair;
+
+public class FireBall extends Entity implements Movable, Animatable{
+
+	public FireBall(Pair pos, Pair size) {
+		super(pos, size);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void changeImage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveX() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getXSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setXSpeed(double xSpeed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
